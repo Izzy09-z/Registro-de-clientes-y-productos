7c5a859948e809f1d38a4a55b6df8a548f93d555
Isabella <uribe.garces.isabella@gmail.com>
2025-06-21 15:39:54 -0500

    Nuevo7

---
+++ DESAFIO 2.py
@@ -0,0 +1,17 @@
+#Comprador de cifras iguales. Pida un numero de 2 cifras si son iguales "Doble", sino muestra la mayor de las dos
+
+# cifra_str = input("Ingresa un numero de dos cifras: ")
+
+# if len(cifra_str) == 2 and cifra_str.isdigit():
+#     cifra = int(cifra_str) 
+#     numer1 = int(cifra_str[0])
+#     numer2 = int(cifra_str[1])
+#     if numer1 == numer2:
+#         print("Doble")
+#     else:
+#         if numer1 > numer2:
+#             print(f"La cifra mayor es: {numer1}")
+#         else:
+#             print(f"La cifra mayor es: {numer2}")
+# else:
+#     print("Por favor ingresa un numero de dos cifras.")
